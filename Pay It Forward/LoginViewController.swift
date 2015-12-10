2812999d24b517bcf8dca076f098807e8df114f3
Manav Gabhawala <manav1907@gmail.com>
2015-12-09 22:44:31 -0500

    Added UI Hooks

---
+++ Pay It Forward/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Pay It Forward
+//
+//  Created by Manav Gabhawala on 12/9/15.
+//  Copyright © 2015 Manav Gabhawala. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController
+{
+	@IBOutlet var webView: UIWebView!
+	
+	override func viewDidAppear(animated: Bool)
+	{
+		super.viewDidAppear(animated)
+	}
+	
+	
+}
--- Pay It Forward/ViewController.swift
+++ Pay It Forward/ViewController.swift
@@ -7,20 +7,32 @@
 //
 
 import UIKit
+import CocoaMultipeer
 
 class ViewController: UIViewController
 {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+	
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	override func viewDidAppear(animated: Bool)
+	{
+		super.viewDidAppear(animated)
+		guard VenmoManager.sharedManager().isLoggedIn
+		else
+		{
+			let controller = storyboard!.instantiateViewControllerWithIdentifier("loginViewController")
+			controller.modalPresentationStyle = .FormSheet
+			presentViewController(controller, animated: true, completion: nil)
+			return
+		}
+	}
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 }
 
